ebb4431d61c44a94f852bae4ffb99e74187d3797
JordanNguyen <Admin@RIKKACHAN>
2017-08-13 22:02:45 +0930

    Changes and editing made to trying to navigate to a new view using action links in the dropdown

--- ICTProject/Controllers/DefaultController.cs
+++ ICTProject/Controllers/DefaultController.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+// JORDAN -- Made this controller to test travelling to new View. (views should be in Default folder)
+// So far nothing working.
 namespace ICTProject.Controllers
 {
     public class DefaultController : Controller
     {
+
         // GET: Default
         public ActionResult Index()
         {
